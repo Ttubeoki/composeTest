a99daf073af148259ec57e2d68a721ce85145368
Ttubeoki <wndnjs19@gmail.com>
2023-05-26 22:36:06 +0900

    1. init

--- app/src/main/java/com/ttubeoki/composetest/MainActivity.kt
+++ app/src/main/java/com/ttubeoki/composetest/MainActivity.kt
@@ -3,12 +3,14 @@ package com.ttubeoki.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.ttubeoki.composetest.ui.theme.ComposeTestTheme
 
@@ -33,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.fillMaxSize().background(Color.Black)
     )
 }
 
